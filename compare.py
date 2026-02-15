6dd93dc35be84f397e9fa3d468bec19f5a46ad52
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-02-15 23:35:15 +0000

    Progress video 2

---
+++ compare.py
@@ -0,0 +1,7 @@
+x = int(input("Whats x? "))
+y = int(input("Whats y? "))
+
+if x != y:
+    print("Not equal")
+else:
+    print("Equal")
---
+++ grade.py
@@ -0,0 +1,12 @@
+score = int(input("Score: "))
+
+if score >= 90:
+    print("Grade super")
+elif score >= 80:
+    print("Grade bad")
+elif score >= 70:
+    print("Grade horrible")
+elif score >= 60:
+    print("Bad")
+else:
+    print("f")

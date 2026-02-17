9dd6f5cdc95e02e726fce0a334c61d63ef576f14
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-02-17 03:15:02 +0000

    Problem 1.5

---
+++ meal.py
@@ -0,0 +1,14 @@
+def main():
+    time = input("What's the time? ")
+    convert(time)
+
+
+def convert(t)
+    t = t.replace(":", "")
+    t = int(t)
+    
+    if 900 >= t >= 700:
+        print("Breakfast")
+    elif 
+
+main()

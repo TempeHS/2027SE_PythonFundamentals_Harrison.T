286d4a267e928bc129490253c6bbaa603000478e
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-02-19 02:47:21 +0000

    problem 1.5 is completed

--- meal.py
+++ meal.py
@@ -3,12 +3,19 @@ def main():
     convert(time)
 
 
-def convert(t)
+def convert(t):
     t = t.replace(":", "")
     t = int(t)
-    
-    if 900 >= t >= 700:
-        print("Breakfast")
-    elif 
 
-main()
+    if 800 >= t >= 700:
+        print("Breakfast")
+    elif 1300 >= t >= 1200:
+        print("Lunch time")
+    elif 1900 >= t >= 1800:
+        print("Dinner time")
+    else:
+        print("No food")
+
+
+if __name__ == "__main__":
+    main()

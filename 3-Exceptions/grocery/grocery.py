25631e34dac871b78a247e251baff259e11f5520
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-03-01 07:45:06 +0000

    Problem 3..3

---
+++ 3-Exceptions/grocery/grocery.py
@@ -0,0 +1,22 @@
+def main():
+
+    grocery_list = {}
+
+    while True:
+        try:
+            item = input().upper()
+
+            if item in grocery_list:
+                grocery_list[item] = grocery_list[item] + 1
+            else:
+                grocery_list[item] = 1
+
+        except EOFError:
+
+            for item in sorted(grocery_list):
+                print(grocery_list[item], item)
+
+            break
+
+
+main()

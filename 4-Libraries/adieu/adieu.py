b3949ad22c73c7ecdadffb36dc3af5af2e3201f4
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-03-06 00:50:58 +0000

    4.3 done

--- 4-Libraries/adieu/adieu.py
+++ 4-Libraries/adieu/adieu.py
@@ -1,12 +1,17 @@
-names = {}
+names = []
 
 while True:
     try:
-        item = input()
+        item = input().title()
+
+        if item.isalpha():
+            names.append(item)
 
     except EOFError:
-
-        for item in names:
-            print(f"{names},and {item}")
-
+        if len(names) == 1:
+            print(f"Adieu, adieu, to {names[0]}")
+        elif len(names) == 2:
+            print(f"Adieu, adieu, to {names[0]} and {names[1]}")
+        elif len(names) > 2:
+            print(f"Adieu, adieu, to {', '.join(names[:-1])}, and {names[-1]}")
         break

f5551746d53059377ba060dbe2ca71c52d95c04b
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-03-08 09:21:51 +0000

    4.4 done

--- 4-Libraries/game/game.py
+++ 4-Libraries/game/game.py
@@ -1,6 +1,30 @@
 import random
 
+while True:
+    try:
+        level = int(input("Level: "))
+        if level < 1:
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid number")
+
+guess = random.randint(1, level)
+
 
 while True:
-    try
-        input
+    try:
+
+        answer = int(input("Guess the number: "))
+
+        if answer > guess:
+            print("Too High!")
+        elif answer < guess:
+            print("Too Low!")
+        else:
+            print("Right on the money")
+            break
+
+    except ValueError:
+        print("Enter a valid integer")
+        break

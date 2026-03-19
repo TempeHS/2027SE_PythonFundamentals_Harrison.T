4c51b5245f8a9aae0119efc2dfde1883a06bcad8
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-03-19 03:15:02 +0000

    22:08 video

---
+++ names.py
@@ -0,0 +1,26 @@
+names = []
+
+with open("names.txt")
+
+
+
+
+
+##with open("names.txt", "r") as file:
+    #for line in file:
+        #print("hello", line.rstrip())
+
+
+## name = input("Whats your name? ")
+
+# with open("names.txt", "a") as file:  ## a for append   w for write
+#   file.write(f"{name}\n")
+
+
+## names = []
+
+# for _ in range(3):
+#    names.append(input("What's your name? "))
+
+# for name in sorted(names):
+#    print(f"hello, {name}")

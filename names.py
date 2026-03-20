9f57211bbecbf1731144f0d10942c7fb353b1373
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-03-20 00:54:32 +0000

    50:00

--- names.py
+++ names.py
@@ -1,14 +1,16 @@
 names = []
 
-with open("names.txt")
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names, reverse=True):
+    print(f"hello, {name}")
 
 
-
-
-
-##with open("names.txt", "r") as file:
-    #for line in file:
-        #print("hello", line.rstrip())
+##with open("names.", "r") as file:
+# for line in file:
+# print("hello", line.rstrip())
 
 
 ## name = input("Whats your name? ")

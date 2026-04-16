7b205a6e60a3268e8f49d6288471aa74af821914
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-04-16 12:13:06 +0000

    Unit test video

--- calculator.py
+++ calculator.py
@@ -1,22 +1,11 @@
-def hello(to="world"):
-    # defines hello, with anything under the colon. "Makes to = world as default value"
-    print("hello,", to)
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
 
 
-hello()
-name = input("Whats your name? ")
-hello(name)
-# Name is being converted into "To" which in the def command the "to" is the "name"
+def square(n):
+    return n * n
 
 
-## x = float(input("What's X? "))     Like an int but more specific, adds decimal points or whatnot
-## y = float(input("What's Y? "))
-
-
-## z = round(x / y, 2)  # round to 2 digits
-# z = x / y
-
-# print(z)
-
-## print(f"{z:.2f}")
-# - If you forget the round, and want to round it by using an f string 2f is how many digits you want to print
+if __name__ == "__main__":
+    main()
--- hello.py
+++ hello.py
@@ -1,26 +1,11 @@
-# ask for their name
-name = input("Whats your name?? ").strip().title()
+def main():
+    name = input("Whats your name? ")
+    print(hello(name))
 
 
-#                                  ⬆️         ⬆️
-# remove whitespace from string/str and capitalize users name
-# name = name.strip().title()
+def hello(to="world"):
+    return f"hello, {to}"
 
 
-# capitalizes the first letter of each word
-## name = name.title()
-
-
-# Say hi to user
-# print("Hi, ", end="")
-# print(name)
-
-
-print(f"hello, {name}")
-
-
-# f = formate the {}
-
-
-# print("hello, /"hello/"")
-# = hello, "hello"
+if __name__ == "__main__":
+    main()

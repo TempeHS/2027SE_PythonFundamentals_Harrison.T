6ee456927593f4fbebe51976d494ea12eb848e1f
Harrison T <159206613+harry-vt@users.noreply.github.com>
2026-04-24 02:49:48 +0000

    1:11:11 Vido

--- student.py
+++ student.py
@@ -1,12 +1,27 @@
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing Name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()
+    print(student)
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 
 if __name__ == "__main__":
